aa3813939710c845f8374d17b5a402a2a926e29f
Chadewik-junao <53118074+Chadewik-junao@users.noreply.github.com>
2021-04-27 21:02:15 +0800

    实体类定义

--- src/main/java/Candidate.java
+++ src/main/java/Candidate.java
@@ -1,3 +1,42 @@
+import java.sql.Date;
+
 //候选者类
 public class Candidate {
+    private String voteId;//投票id
+    private String candidateId;//候选人编号
+    private String candidateName;//候选人名字
+
+    public void setVoteId(String voteId) {
+        this.voteId = voteId;
+    }
+
+    public void setCandidateId(String candidateId) {
+        this.candidateId = candidateId;
+    }
+
+    public void setCandidateName(String candidateName) {
+        this.candidateName = candidateName;
+    }
+
+    public void setVotes(int votes) {
+        this.votes = votes;
+    }
+
+    private int votes;//当前候选人获得票数
+
+    public String getVoteId() {
+        return voteId;
+    }
+
+    public String getCandidateId() {
+        return candidateId;
+    }
+
+    public String getCandidateName() {
+        return candidateName;
+    }
+
+    public int getVotes() {
+        return votes;
+    }
 }
--- src/main/java/Vote.java
+++ src/main/java/Vote.java
@@ -1,4 +1,51 @@
+import java.sql.Date;
+
 //投票类
 public class Vote {
+    private String voteId;//投票id
+    private String voteName;//投票名
+    private Date startDate, StopDate;//起止时间
+    private int allVotes;//总票数
+
+    public String getVoteId() {
+        return voteId;
+    }
+
+    public void setVoteId(String voteId) {
+        this.voteId = voteId;
+    }
+
+    public String getVoteName() {
+        return voteName;
+    }
+
+    public void setVoteName(String voteName) {
+        this.voteName = voteName;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getStopDate() {
+        return StopDate;
+    }
+
+    public void setStopDate(Date stopDate) {
+        StopDate = stopDate;
+    }
+
+    public int getAllVotes() {
+        return allVotes;
+    }
+
+    public void setAllVotes(int allVotes) {
+        this.allVotes = allVotes;
+    }
+
 
 }
